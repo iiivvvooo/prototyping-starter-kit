d9646a71d769673cdc08800cb0ecd2516e06e377
Ivo Jacobs <mail@ivojacobs.de>
2016-11-12 19:29:49 +0100

    Refactoring
    New file structure and gulpfile cleanup

--- README.md
+++ README.md
@@ -4,11 +4,6 @@ Kickstart your frontend development workflow with [Gulp](http://gulpjs.com/), [N
 ![Browser Screenshot](https://raw.githubusercontent.com/ivojacobs/frontend-kickstarter/master/screenshot.png)
 
 ## Get Started
-* Install dependencies: ```npm install --global gulp-cli bower```
-* Clone the repository: ```git clone https://github.com/ivojacobs/frontend-kickstarter.git```
-* Run ```npm install && bower install```
-* Run ```gulp serve``` to preview and watch for changes
-* Run ```bower install --save <package>``` to install frontend dependencies
-* Run ```gulp serve:test``` to run the tests in the browser
-* Run ```gulp``` to build your webapp for production
-* Run ```gulp serve:dist``` to preview the production build
+* Clone repository
+* Run ```npm install```
+* Run ```gulp serve``` or ```npm run dev``` to start developing
--- gulpfile.js
+++ gulpfile.js
@@ -1,206 +1,107 @@
-// generated on 2016-09-15 using generator-webapp 2.1.0
+
+'use strict';
+
 const gulp = require('gulp');
+const fs = require('fs');
+const path = require('path');
+const del = require('del');
+const runSequence = require('run-sequence');
 const gulpLoadPlugins = require('gulp-load-plugins');
 const browserSync = require('browser-sync');
-const del = require('del');
-const fs = require('fs');
-const wiredep = require('wiredep').stream;
 
 const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
 
-gulp.task('styles', () => {
-  return gulp.src('app/styles/*.scss')
+gulp.task('assets', () =>
+  gulp
+    .src('src/assets/**/*')
+    .pipe(gulp.dest('dist/assets'))
+);
+
+gulp.task('favicon', () =>
+  gulp
+    .src([
+      'src/favicon*.*'
+    ], { base: 'src' })
+    .pipe(gulp.dest('dist'))
+);
+
+// Get data from the corresponding filename
+// e.g. inject data/foo.json into foo.html
+const getData = (file) => {
+  const dataPath = path.resolve('./src/views/data/' + path.basename(file.path, '.html') + '.json')
+  let data = {};
+
+  try {
+    data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+  } catch(e) {
+    // Don't fail if the JSON is badly formed or the file doesn't exist
+  } finally {
+    return data;
+  }
+};
+
+gulp.task('views', () =>
+  gulp
+    .src([
+      'src/views/**/*.html',
+      '!src/views/**/_*.html'
+    ], { base: 'src/views' })
+    .pipe($.data(getData))
+    .pipe($.nunjucks.compile())
+    .pipe(gulp.dest('dist'))
+);
+
+gulp.task('scripts', () =>
+  gulp
+    .src([
+      'src/scripts/**/*.js'
+    ], { base: 'src' })
+    .pipe(gulp.dest('dist/'))
+);
+
+gulp.task('styles', () =>
+  gulp
+    .src('src/styles/*.scss')
     .pipe($.plumber())
     .pipe($.sourcemaps.init())
     .pipe($.sass.sync({
-      outputStyle: 'expanded',
-      precision: 10,
-      includePaths: ['.']
+      precision: 10
     }).on('error', $.sass.logError))
-    .pipe($.autoprefixer({browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']}))
-    .pipe($.sourcemaps.write())
-    .pipe(gulp.dest('.tmp/styles'))
-    .pipe(reload({stream: true}));
-});
-
-gulp.task('scripts', () => {
-  return gulp.src('app/scripts/**/*.js')
-    .pipe($.plumber())
-    .pipe($.sourcemaps.init())
-    .pipe($.babel())
+    .pipe($.autoprefixer({ browsers: ['last 2 versions'], remove: false }))
     .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest('.tmp/scripts'))
-    .pipe(reload({stream: true}));
-});
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(browserSync.stream({ match: '**/*.css' }))
+);
 
-function lint(files, options) {
-  return gulp.src(files)
-    .pipe(reload({stream: true, once: true}))
-    .pipe($.eslint(options))
-    .pipe($.eslint.format())
-    .pipe($.if(!browserSync.active, $.eslint.failAfterError()));
-}
-
-gulp.task('lint', () => {
-  return lint('app/scripts/**/*.js', {
-    fix: true
-  })
-    .pipe(gulp.dest('app/scripts'));
-});
-gulp.task('lint:test', () => {
-  return lint('test/spec/**/*.js', {
-    fix: true,
-    env: {
-      jasmine: true
-    }
-  })
-    .pipe(gulp.dest('test/spec/**/*.js'));
-});
-
-gulp.task('views', () => {
-  return gulp.src('app/*.njk')
-  .pipe($.data(function(file) {
-    return JSON.parse(fs.readFileSync('app/_data/data.json'));
-  }))
-  .pipe($.nunjucksRender({
-      path: 'app',
-      watch: false
-    }))
-    .pipe(gulp.dest('.tmp'))
-});
-
-gulp.task('html', ['views', 'styles', 'scripts'], () => {
-  return gulp.src(['app/*.html', '.tmp/*.html'])
-    .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
-    .pipe($.if('*.js', $.uglify()))
-    .pipe($.if('*.css', $.cssnano({safe: true, autoprefixer: false})))
-    //.pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('images', () => {
-  return gulp.src('app/images/**/*')
-    .pipe($.cache($.imagemin({
-      progressive: true,
-      interlaced: true,
-      // don't remove IDs from SVGs, they are often used
-      // as hooks for embedding and styling
-      svgoPlugins: [{cleanupIDs: false}]
-    })))
-    .pipe(gulp.dest('dist/images'));
-});
-
-gulp.task('fonts', () => {
-  return gulp.src(require('main-bower-files')('**/*.{eot,svg,ttf,woff,woff2}', function (err) {})
-    .concat('app/fonts/**/*'))
-    .pipe(gulp.dest('.tmp/fonts'))
-    .pipe(gulp.dest('dist/fonts'));
-});
-
-gulp.task('extras', () => {
-  return gulp.src([
-    'app/*.*',
-    '!app/*.html',
-    '!app/*.njk'
-  ], {
-    dot: true
-  }).pipe(gulp.dest('dist'));
-});
-
-gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
-
-gulp.task('serve', ['views', 'styles', 'scripts', 'fonts'], () => {
+gulp.task('default', ['build'], () => {
   browserSync({
     notify: false,
-    port: 9000,
-    server: {
-      baseDir: ['.tmp', 'app'],
-      routes: {
-        '/bower_components': 'bower_components'
-      }
-    }
+    server: 'dist'
   });
 
+  gulp.watch('src/styles/*.scss', ['styles']);
   gulp.watch([
-    'app/*.html',
-    '.tmp/*.html',
-    'app/_data/*.json',
-    'app/images/**/*',
-    '.tmp/fonts/**/*'
-  ]).on('change', reload);
+    'src/views/**/*.html',
+    'src/views/data/*.json'
+  ], ['views']);
+  gulp.watch('src/assets/**/*.{woff,woff2,txt,jpg,png,gif,svg}', ['assets']);
+  gulp.watch('src/scripts/**/*.js', ['scripts']);
+  gulp.watch([
+    'dist/**/*.html',
+    'dist/scripts/**/*.js',
 
-  gulp.watch('app/**/*.html', ['views']);
-  gulp.watch('app/**/*.njk', ['views']);
-  gulp.watch('app/**/*.json', ['views']);
-  gulp.watch('app/styles/**/*.scss', ['styles']);
-  gulp.watch('app/scripts/**/*.js', ['scripts']);
-  gulp.watch('app/fonts/**/*', ['fonts']);
-  gulp.watch('bower.json', ['wiredep', 'fonts']);
+    // Note: we're not watching icons and fonts changes,
+    // as they're slowing down the task
+    'dist/assets/*.{woff,woff2,txt,jpg,png,gif,svg}',
+    'dist/assets/styles/*.css'
+  ]).on('change', browserSync.reload);
 });
 
-gulp.task('serve:dist', () => {
-  browserSync({
-    notify: false,
-    port: 9000,
-    server: {
-      baseDir: ['dist']
-    }
-  });
-});
+gulp.task('clean', () => del(['dist'], { dot: true }));
 
-gulp.task('serve:test', ['scripts'], () => {
-  browserSync({
-    notify: false,
-    port: 9000,
-    ui: false,
-    server: {
-      baseDir: 'test',
-      routes: {
-        '/scripts': '.tmp/scripts',
-        '/bower_components': 'bower_components'
-      }
-    }
-  });
-
-  gulp.watch('app/scripts/**/*.js', ['scripts']);
-  gulp.watch('test/spec/**/*.js').on('change', reload);
-  gulp.watch('test/spec/**/*.js', ['lint:test']);
-});
-
-// inject bower components
-gulp.task('wiredep', () => {
-  gulp.src('app/styles/*.scss')
-    .pipe(wiredep({
-      ignorePath: /^(\.\.\/)+/
-    }))
-    .pipe(gulp.dest('app/styles'));
-
-  gulp.src('app/layouts/*.njk')
-    .pipe(wiredep({
-      exclude: ['bootstrap-sass'],
-      ignorePath: /^(\.\.\/)*\.\./,
-       fileTypes: {
-         njk: {
-           block: /(([ \t]*)<!--\s*bower:*(\S*)\s*-->)(\n|\r|.)*?(<!--\s*endbower\s*-->)/gi,
-           detect: {
-             js: /<script.*src=['"]([^'"]+)/gi,
-             css: /<link.*href=['"]([^'"]+)/gi
-           },
-           replace: {
-             js: '<script src="{{filePath}}"></script>',
-             css: '<link rel="stylesheet" href="{{filePath}}" />'
-           }
-         }
-       }
-    }))
-    .pipe(gulp.dest('app/layouts'));
-});
-
-gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
-  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
-});
-
-gulp.task('default', ['clean'], () => {
-  gulp.start('build');
+gulp.task('build', callback => {
+  runSequence(
+    'clean', [ 'assets', 'views', 'styles', 'scripts', 'favicon' ],
+  callback);
 });
